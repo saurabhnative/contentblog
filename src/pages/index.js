9dda5c390c1fb735156628615c48226a3a51e1a9
Saurabh Native <codeclassifiers@gmail.com>
2020-06-06 16:40:39 +0530

    second

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import Button from "../components/button"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Gatsby Starter Personal Website"
+    const siteTitle = "Codeclassifiers Personal Blog"
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -22,12 +22,10 @@ class IndexPage extends React.Component {
             👋
           </span>
         </h1>
-        <p>Welcome to your new Gatsby website. You are on your home page.</p>
+        <p>Welcome to my personal blog. You are on your home page.</p>
         <p>
-          This starter comes out of the box with styled components and Gatsby's
-          default starter blog running on Netlify CMS.
+          Check out my blog section
         </p>
-        <p>Now go build something great!</p>
         <Link to="/blog/">
           <Button marginTop="35px">Go to Blog</Button>
         </Link>
