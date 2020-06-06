3b1f603be341734b4153c912dd44d310fed961b0
Saurabh Native <codeclassifiers@gmail.com>
2020-06-06 16:46:51 +0530

    second

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,12 +1,12 @@
 module.exports = {
   siteMetadata: {
     // edit below
-    title: `Gatsby Starter Personal Blog`,
-    author: `Gatsby`,
-    description: `A starter personal blog with styled components, dark mode, and Netlify CMS.`,
+    title: `Codeclassifiers Personal Blog`,
+    author: `Codeclassifiers`,
+    description: `A personal blog ceovering articles on data science and machine learning`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `gatsbyjs`,
+      twitter: `codeclassifiers`,
     },
   },
   plugins: [
