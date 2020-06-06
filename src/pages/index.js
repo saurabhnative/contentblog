b68372f1084bac139af39770d78a61d38274dc35
Saurabh Native <codeclassifiers@gmail.com>
2020-06-06 17:26:12 +0530

    third

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,13 @@ import { Link } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Button from "../components/button"
+import { rhythm } from "../utils/typography"
 
 class IndexPage extends React.Component {
   render() {
-    const siteTitle = "Codeclassifiers Personal Blog"
+    const siteTitle = "Codeclassifiers"
+    const { data } = this.props
+    const posts = data.allMdx.edges
 
     return (
       <Layout location={this.props.location} title={siteTitle}>
@@ -15,14 +18,49 @@ class IndexPage extends React.Component {
           title="Home"
           keywords={[`blog`, `gatsby`, `javascript`, `react`]}
         />
-        <img style={{ margin: 0 }} src="./GatsbyScene.svg" alt="Gatsby Scene" />
         <h1>
           Hey people{" "}
           <span role="img" aria-label="wave emoji">
             👋
           </span>
         </h1>
-        <p>Welcome to my personal blog. You are on your home page.</p>
+        <p>
+          Welcome to my personal blog. <br></br>
+          Here I will try to cover articles on data science and web development.
+        </p>
+        <p>
+          You can get in touch with me on   
+          <a href="https://www.linkedin.com/in/saurabh-mhatre/"> Linkedin </a> 
+          or <a href="https://twitter.com/saurabhnative"> Twitter </a>
+        </p>
+        <h2>Here's a list of some of my lastest posts:</h2>
+        <div style={{ margin: "20px 0 40px" }}>
+          {posts.map(({ node }) => {
+            const title = node.frontmatter.title || node.fields.slug
+            return (
+              <div key={node.fields.slug}>
+                <h3
+                  style={{
+                    marginBottom: rhythm(1 / 4),
+                  }}
+                >
+                  <Link
+                    style={{ boxShadow: `none` }}
+                    to={`blog${node.fields.slug}`}
+                  >
+                    {title}
+                  </Link>
+                </h3>
+                <small>{node.frontmatter.date}</small>
+                <p
+                  dangerouslySetInnerHTML={{
+                    __html: node.frontmatter.description || node.excerpt,
+                  }}
+                />
+              </div>
+            )
+          })}
+        </div>
         <p>
           Check out my blog section
         </p>
@@ -33,5 +71,31 @@ class IndexPage extends React.Component {
     )
   }
 }
+
+export const pageQuery = graphql`
+  query {
+    site {
+      siteMetadata {
+        title
+      }
+    }
+    allMdx(sort: { fields: [frontmatter___date], order: DESC }) {
+      edges {
+        node {
+          excerpt
+          fields {
+            slug
+          }
+          frontmatter {
+            date(formatString: "MMMM DD, YYYY")
+            title
+            description
+          }
+        }
+      }
+    }
+  }
+`
+
 
 export default IndexPage
