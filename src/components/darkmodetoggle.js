bff619d730db07df4d7579f170848d4d4a1ec309
Saurabh Native <codeclassifiers@gmail.com>
2020-06-20 20:07:07 +0530

    Set theme to light by default

--- src/components/darkmodetoggle.js
+++ src/components/darkmodetoggle.js
@@ -3,6 +3,13 @@ import { ThemeToggler } from 'gatsby-plugin-dark-mode'
 import styled from "styled-components"
 
 class DarkModeToggle extends React.Component {
+  componentDidMount() {
+    if(localStorage.getItem('theme')){
+        window.__setPreferredTheme(localStorage.getItem('theme'))
+    } else {
+        window.__setPreferredTheme('light')
+    }
+  }
   render() {
     return (
       <ThemeToggler>
@@ -12,6 +19,7 @@ class DarkModeToggle extends React.Component {
               type="checkbox"
               onChange={e => toggleTheme(e.target.checked ? 'dark' : 'light')}
               checked={theme === 'dark'}
+              defaultChecked={false}
             />{' '}
             Dark mode
           </ToggleLabel>
