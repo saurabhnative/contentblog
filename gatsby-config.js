1e07d4766db4d6a487d24c3fa60a31e15d0f346b
codeclassifiers <codeclassifiers@gmail.com>
2020-08-12 21:32:51 +0530

    github gist added

--- gatsby-config.js
+++ gatsby-config.js
@@ -101,5 +101,25 @@ module.exports = {
         environments: ['production', 'development']
       },
     },
+    {
+      resolve: "gatsby-transformer-remark",
+      options: {
+        plugins: [
+          {
+            resolve: "gatsby-remark-embed-gist",
+            options: {
+              // Optional:
+    
+              // the github handler whose gists are to be accessed
+              username: "codeclassifiers",
+    
+              // a flag indicating whether the github default gist css should be included or not
+              // default: true
+              gistDefaultCssInclude: true,
+            }
+          }
+        ]
+      }
+    }
   ],
 }
