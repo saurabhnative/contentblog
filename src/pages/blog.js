95ac4be757786f4f01e7955b45d9982564053632
Saurabh Mhatre <codeclassifiers@gmail.com>
2021-01-02 20:02:36 +0530

    Article id added

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -29,6 +29,7 @@ class Blog extends React.Component {
                   <Link
                     style={{ boxShadow: `none` }}
                     to={`blog${node.fields.slug}`}
+                    id="article-title"
                   >
                     {title}
                   </Link>
